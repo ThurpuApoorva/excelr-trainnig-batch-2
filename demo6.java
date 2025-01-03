92984d5377031a8f37fc7c834020fe55cc324868
ThurpuApoorva <apoorvar757@gmail.com>
2025-01-03 10:21:54 +0530

    Add files via upload

---
+++ demo6.java
@@ -0,0 +1,11 @@
+
+public class demo6{
+    public static void main(String[] args) {
+        String username = "Apoorva";
+        String password ="Apoorva@123";
+        while((username == "Apoorva") && (password == "Apoorva@123")) {
+            System.out.println("welcome");
+            break;
+        }
+    }
+    }
