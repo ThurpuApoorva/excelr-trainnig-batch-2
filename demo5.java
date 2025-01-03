5fdf711ef11c7071d300848a31c3fbbb5de5b07a
ThurpuApoorva <apoorvar757@gmail.com>
2025-01-03 10:02:39 +0530

    Add files via upload

---
+++ demo5.java
@@ -0,0 +1,11 @@
+
+public class demo5 {
+    public static void main(String[] args) {
+        String username = "user001";
+        String pud = "user_001";
+        while((username == "user001") && (pud == "user_001")) {
+            System.out.println("hello user Happy new year");
+            break;
+        }
+    }
+    }
